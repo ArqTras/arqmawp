c3845d7864898e05456dcd0eb47b12f2d759de77
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-02-08 13:26:35 +0100

    s

--- arqma-woocommerce-gateway.php
+++ arqma-woocommerce-gateway.php
@@ -18,7 +18,7 @@ define('ARQMA_GATEWAY_TESTNET_EXPLORER_URL', 'https://blocks.arqma.com/');
 define('ARQMA_GATEWAY_ADDRESS_PREFIX', 0x2cca);
 define('ARQMA_GATEWAY_ADDRESS_PREFIX_INTEGRATED', 0x116bc7);
 define('ARQMA_GATEWAY_ATOMIC_UNITS', 9);
-define('ARQMA_GATEWAY_ATOMIC_UNIT_THRESHOLD', 8); // Amount under in atomic units payment is valid
+define('ARQMA_GATEWAY_ATOMIC_UNIT_THRESHOLD', 11); // Amount under in atomic units payment is valid
 define('ARQMA_GATEWAY_DIFFICULTY_TARGET', 120);
 
 // Do not edit these constants
